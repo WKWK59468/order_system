52bf2a7cd43a2d801b9344fca1f1b06946b8e0ae
jrong <wkwk59468@gmail.com>
2021-12-08 20:33:45 +0800

    修改回傳格式

--- function_package.js
+++ function_package.js
@@ -1,8 +1,32 @@
 module.exports = {
-    res_type: (status, content) => {
+    statusCode: {
+        "ok": 200,
+        "created": 201,
+        "noContent": 204,
+        "badRequest": 400,
+        "unauthorized": 401,
+        "forbidden": 403,
+        "notFound": 404,
+        "ServerError": 500
+    },
+    res_type: (status, message, payload) => {
         return {
             "status": status,
-            "content": content
+            "message": message,
+            "payload": payload
         }
     }
+    /* 範例格式
+        {
+            "status":200,
+
+            //obj、null
+            "payload":{
+                "功能名稱_type":"single、multiple、All",
+                "功能名稱_自訂":"accessToken"
+            },
+
+            "message":"msgString"
+        } 
+    */
 }
