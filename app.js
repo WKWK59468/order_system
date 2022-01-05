92a945023198f6dc5bcd8b8d06cc5893b48ba85b
jrong <wkwk59468@gmail.com>
2022-01-05 13:40:53 +0800

    新增example.env檔及connect.js位置變更到主目錄

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser")
 const logger = require("morgan")
 const cors = require("cors")
 const index = require("./routes/index.js")
-const DB = require("./models/connect")
+const DB = require("./connect")
 
 const app = express()
 
