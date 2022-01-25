5b0d39abb6582f37198b93e2856d26a5a7e7a3a8
marian <marianstyles16888@gmail.com>
2022-01-25 10:06:55 +0800

    jwt測試完成

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const userModels = require("../models/user.model")
 const Package = require("../function_package")
 const jwt = require("jsonwebtoken")
@@ -17,13 +18,15 @@ class LoginController {
           .compare(password, result.password)
           .then((check) => {
             if (check) {
-              let jwt_token = jwt.sign({}, SECRET, { expiresIn: "1 day" })
+              let jwt_token = jwt.sign({ email: email, _id: result._id, name: result.name, role: result.role }, SECRET, { expiresIn: "1 day" })
               res.status(200).json(Package.res_type(200, "OK", jwt_token))
             } else {
+              console.log("PasswordError")
               res.status(400).json(Package.res_type(400, "PasswordError", null))
             }
           })
           .catch((err) => {
+            console.log(err)
             res.status(400).json(Package.res_type(400, "PasswordError", null))
           })
       })
@@ -34,6 +37,17 @@ class LoginController {
           res.status(500).json(Package.res_type(500, "ServerError", err))
         }
       })
+  }
+  check = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '')
+    jwt.verify(token, SECRET, (jwterr, decoded) => {
+      if (jwterr) {
+        res.status(401).json({ result: "not login", err: jwterr })
+      } else {
+        req.userData = { "decoded": decoded }
+        next()
+      }
+    })
   }
 }
 
--- routes/login.route.js
+++ routes/login.route.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const fun = require("../function_package")
 const LoginController = require("../controllers/login.controller")
 
 router.post("/", LoginController.login)
+router.post("/check", LoginController.check, function (req, res) {
+    res.status(200).json(fun.res_type(200, "Login Success", null))
+})
 
 module.exports = router
