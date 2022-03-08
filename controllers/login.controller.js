bd9405004ba251c0c00af0bd0e716e1e4392ed0a
marian <marianstyles16888@gmail.com>
2022-03-08 11:13:29 +0800

    依登入方式拆功能函式

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,36 +5,121 @@ const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 const SECRET = process.env.SECRET
 
+function origin(res, email, password) {
+  userModels
+    .fetchOne(email)
+    .then((result) => {
+      bcrypt
+        .compare(password, result.password)
+        .then((check) => {
+          if (check) {
+            let jwt_token = jwt.sign({ email: email, _id: result._id, name: result.name, role: result.role }, SECRET, { expiresIn: "1 day" })
+            res.status(200).json(fun.res_type(200, "OK", jwt_token))
+          } else {
+            res.status(400).json(fun.res_type(400, "PasswordError", null))
+          }
+        })
+        .catch((err) => {
+          res.status(400).json(fun.res_type(400, "LoginError", null))
+        })
+    })
+    .catch((err) => {
+      if (err === "查無此Email") {
+        res.status(400).json(fun.res_type(400, "EmailError", null))
+      } else {
+        res.status(500).json(fun.res_type(500, "ServerError", err))
+      }
+    })
+}
+
+function google(res, email, googleUsername, googleUserID) {
+  userModels
+    .fetchOne(email)
+    .then((result) => {
+      bcrypt
+        .compare(googleUserID, result.googleUserID)
+        .then((check) => {
+
+        })
+        .catch((err) => {
+
+        })
+    })
+    .catch((err) => {
+      if (err === "查無此Email") {
+
+        const data = {
+          name: googleUsername,
+          googleUserID: googleUserID,
+          email: email,
+        }
+
+        userModels.addUser(data)
+
+      } else {
+        res.status(500).json(fun.res_type(500, "ServerError", err))
+      }
+    })
+}
+
+function line(res, lineUsername, lineUserID) {
+  userModels
+    .fetchOne(email)
+    .then((result) => {
+      bcrypt
+        .compare(googleUserID, result.googleUserID)
+        .then((check) => {
+
+        })
+        .catch((err) => {
+
+        })
+    })
+    .catch((err) => {
+      if (err === "查無此Email") {
+
+        const data = {
+          name: lineUsername,
+          googleUserID: lineUserID,
+        }
+
+        userModels.addUser(data)
+
+      } else {
+        res.status(500).json(fun.res_type(500, "ServerError", err))
+      }
+    })
+}
+
 class LoginController {
   login = (req, res) => {
     const body = req.body
-    const email = body.email
-    const password = body.password
 
-    userModels
-      .fetchOne(email)
-      .then((result) => {
-        bcrypt
-          .compare(password, result.password)
-          .then((check) => {
-            if (check) {
-              let jwt_token = jwt.sign({ email: email, _id: result._id, name: result.name, role: result.role }, SECRET, { expiresIn: "1 day" })
-              res.status(200).json(fun.res_type(200, "OK", jwt_token))
-            } else {
-              res.status(400).json(fun.res_type(400, "PasswordError", null))
-            }
-          })
-          .catch((err) => {
-            res.status(400).json(fun.res_type(400, "LoginError", null))
-          })
-      })
-      .catch((err) => {
-        if (err === "查無此Email") {
-          res.status(400).json(fun.res_type(400, "EmailError", null))
-        } else {
-          res.status(500).json(fun.res_type(500, "ServerError", err))
-        }
-      })
+    if (body.loginType === "original") {
+      const email = body.email
+      const password = body.password
+
+      origin(res, email, password)
+
+    } else if (body.loginType === "line") {
+      const email = body.email
+      const lineUsername = body.lineUsername
+      const lineUserID = body.lineUserID
+      const password = 123
+
+      line(res, email, lineUsername, lineUserID)
+
+    } else if (body.loginType === "google") {
+      const email = body.email
+      const googleUsername = body.googleUsername
+      const googleUserID = body.googleUserID
+      const password = 123
+
+      google(res, email, googleUsername, googleUserID)
+
+    } else {
+      res.status(400).json(fun.res_type(400, "loginType Error", null))
+    }
   }
   check = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '')
